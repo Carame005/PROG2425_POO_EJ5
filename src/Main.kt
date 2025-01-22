1e364a3df1d67c4fc0c822aa41bc85875fac92a1
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-22 16:35:03 +0100

    EJERCICIO 5 actualizado 2

--- src/Main.kt
+++ src/Main.kt
@@ -23,6 +23,14 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
         require((hora in 0..23) || (hora == 24 && min == 0 && seg == 0)){"Hora no válida (max 24:00:00)"}
     }
 
+    fun pedirTiempo(msj : String): Int? {
+        println(msj)
+        val tiempo = readLine()
+        val number = tiempo?.toInt()
+
+        return number
+    }
+
     fun incrementar(t : Tiempo): Boolean{
         var horaFinal = hora + t.hora
         var minFinal = min + t.min
@@ -47,6 +55,62 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
             return true
         }
 
+    }
+
+    fun decrementar(t: Tiempo): Boolean {
+        val totalSegundos = aSegundos() - t.aSegundos()
+        return if (totalSegundos >= 0) {
+            val nuevoTiempo = desdeSegundos(totalSegundos)
+            hora = nuevoTiempo.hora
+            min = nuevoTiempo.min
+            seg = nuevoTiempo.seg
+            true
+        } else {
+            false
+        }
+    }
+
+    fun comparar(t: Tiempo): Int {
+        return this.aSegundos().compareTo(t.aSegundos())
+    }
+
+    fun copiar(): Tiempo {
+        return Tiempo(hora, min, seg)
+    }
+
+    fun copiar(t: Tiempo) {
+        hora = t.hora
+        min = t.min
+        seg = t.seg
+    }
+
+    fun sumar(t: Tiempo): Tiempo? {
+        val nuevoTiempo = Tiempo(hora + t.hora, min + t.min, seg + t.seg)
+        return if (nuevoTiempo.hora < 24) nuevoTiempo else null
+    }
+
+    fun restar(t: Tiempo): Tiempo? {
+        val totalSegundos = aSegundos() - t.aSegundos()
+        return if (totalSegundos >= 0) desdeSegundos(totalSegundos) else null
+    }
+
+    fun esMayorQue(t: Tiempo): Boolean {
+        return this.aSegundos() > t.aSegundos()
+    }
+
+    fun esMenorQue(t: Tiempo): Boolean {
+        return this.aSegundos() < t.aSegundos()
+    }
+
+    private fun aSegundos(): Int {
+        return hora * 3600 + min * 60 + seg
+    }
+
+    private fun desdeSegundos(segundos: Int): Tiempo {
+        val h = segundos / 3600
+        val m = (segundos % 3600) / 60
+        val s = segundos % 60
+        return Tiempo(h, m, s)
     }
 
     override fun toString(): String {
