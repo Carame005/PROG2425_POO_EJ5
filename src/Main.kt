07a74a825cca03249d62ed2a70fc89469083f77f
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-31 16:37:48 +0100

    EJERCICIO 5 actualizado 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,60 +1,51 @@
 import kotlin.math.min
 
-class Tiempo(var hora: Int, var min : Int, var seg : Int){
+class Tiempo(var hora: Int, var min: Int, var seg: Int) {
 
-    constructor(hora: Int) : this(hora,  0, 0)
+    constructor(hora: Int) : this(hora, 0, 0)
+    constructor(hora: Int, min: Int) : this(hora, min, 0)
 
-    constructor(hora : Int,min : Int) : this(hora, min, 0)
+    init {
+        require(min >= 0) { "Min debe ser positivo o cero!" }
+        require(seg >= 0) { "Seg debe ser positivo o cero!" }
+        require(hora >= 0) { "Hora debe ser positivo o cero!" }
 
-    init{
-        require(min >= 0){"Min dee ser positivo o cero!"}
-        require(seg >= 0){"Seg dee ser positivo o cero!"}
-        require(hora >= 0){"Hora dee ser positivo o cero!"}
-
-        if (seg > 59){
+        if (seg > 59) {
             min += (seg / 60)
             seg %= 60
         }
 
-        if (min > 59){
+        if (min > 59) {
             hora += (min / 60)
             min %= 60
         }
-        require((hora in 0..23) || (hora == 24 && min == 0 && seg == 0)){"Hora no válida (max 24:00:00)"}
+
+        require((hora in 0..23) || (hora == 24 && min == 0 && seg == 0)) { "Hora no válida (máx 24:00:00)" }
     }
 
-    fun pedirTiempo(msj : String): Int? {
-        println(msj)
-        val tiempo = readLine()
-        val number = tiempo?.toInt()
-
-        return number
-    }
-
-    fun incrementar(t : Tiempo): Boolean{
+    fun incrementar(t: Tiempo): Boolean {
         var horaFinal = hora + t.hora
         var minFinal = min + t.min
         var segFinal = seg + t.seg
 
-        if (segFinal > 59){
+        if (segFinal > 59) {
             minFinal += (segFinal / 60)
             segFinal %= 60
         }
 
-        if (minFinal > 59){
+        if (minFinal > 59) {
             horaFinal += (minFinal / 60)
             minFinal %= 60
         }
 
-        if (horaFinal >= 24){
-            return false
-        }else{
+        return if (horaFinal > 24 || (horaFinal == 24 && (minFinal > 0 || segFinal > 0))) {
+            false
+        } else {
             hora = horaFinal
             min = minFinal
             seg = segFinal
-            return true
+            true
         }
-
     }
 
     fun decrementar(t: Tiempo): Boolean {
@@ -70,13 +61,9 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
         }
     }
 
-    fun comparar(t: Tiempo): Int {
-        return this.aSegundos().compareTo(t.aSegundos())
-    }
+    fun comparar(t: Tiempo): Int = this.aSegundos().compareTo(t.aSegundos())
 
-    fun copiar(): Tiempo {
-        return Tiempo(hora, min, seg)
-    }
+    fun copiar(): Tiempo = Tiempo(hora, min, seg)
 
     fun copiar(t: Tiempo) {
         hora = t.hora
@@ -86,7 +73,7 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
 
     fun sumar(t: Tiempo): Tiempo? {
         val nuevoTiempo = Tiempo(hora + t.hora, min + t.min, seg + t.seg)
-        return if (nuevoTiempo.hora < 24) nuevoTiempo else null
+        return if (nuevoTiempo.hora < 24 || (nuevoTiempo.hora == 24 && nuevoTiempo.min == 0 && nuevoTiempo.seg == 0)) nuevoTiempo else null
     }
 
     fun restar(t: Tiempo): Tiempo? {
@@ -94,17 +81,11 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
         return if (totalSegundos >= 0) desdeSegundos(totalSegundos) else null
     }
 
-    fun esMayorQue(t: Tiempo): Boolean {
-        return this.aSegundos() > t.aSegundos()
-    }
+    fun esMayorQue(t: Tiempo): Boolean = this.aSegundos() > t.aSegundos()
 
-    fun esMenorQue(t: Tiempo): Boolean {
-        return this.aSegundos() < t.aSegundos()
-    }
+    fun esMenorQue(t: Tiempo): Boolean = this.aSegundos() < t.aSegundos()
 
-    private fun aSegundos(): Int {
-        return hora * 3600 + min * 60 + seg
-    }
+    private fun aSegundos(): Int = hora * 3600 + min * 60 + seg
 
     private fun desdeSegundos(segundos: Int): Tiempo {
         val h = segundos / 3600
@@ -119,17 +100,22 @@ class Tiempo(var hora: Int, var min : Int, var seg : Int){
 }
 
 
+fun pedirTiempo(msj: String): Int {
+    print("$msj: ")
+    return readLine()?.toIntOrNull() ?: 0
+}
 
-
-fun main(){
-
+fun main() {
     val hora = pedirTiempo("Introduzca la hora")
-    val min = pedirTiempo("Introduzca el min")
-    val seg = pedirTiempo("Introduzca el seg")
+    val min = pedirTiempo("Introduzca el minuto")
+    val seg = pedirTiempo("Introduzca los segundos")
 
     val tiempo1 = Tiempo(hora, min, seg)
-
     val tiempo2 = Tiempo(23, 46, 48)
 
-    tiempo1.incrementar(tiempo2)
+    if (tiempo1.incrementar(tiempo2)) {
+        println("Tiempo actualizado: $tiempo1")
+    } else {
+        println("No se pudo incrementar, supera las 24 horas.")
+    }
 }
